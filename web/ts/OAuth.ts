4d3153ac2d7f4f2ab970dca6a0e9e4edeff6197d
steven <sts321@lehigh.edu>
2019-11-03 17:50:12 -0500

    got the oauth flow to work

---
+++ web/ts/OAuth.ts
@@ -0,0 +1,37 @@
+
+/*
+ * Create form to request access token from Google's OAuth 2.0 server.
+ */
+export function oauthSignIn() {
+    // Google's OAuth 2.0 endpoint for requesting an access token
+    var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+  
+    // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+    var form = document.createElement('form');
+    form.setAttribute('method', 'GET'); // Send as a GET request.
+    form.setAttribute('action', oauth2Endpoint);
+  
+    // Parameters to pass to OAuth 2.0 endpoint.
+    var params = {'client_id': '755594120508-vajdss1hrkqi335sukhur5qaa70s5jji.apps.googleusercontent.com',
+                  'redirect_uri': 'https://clowns-who-code.herokuapp.com ',
+                  'response_type': 'token',
+                  'scope': 'https://www.googleapis.com/auth/drive.appfolder',
+                  'include_granted_scopes': 'true',
+                  'state': 'pass-through value'};
+  
+    // Add form parameters as hidden input values.
+    for (var p in params) {
+      var input = document.createElement('input');
+      input.setAttribute('type', 'hidden');
+      input.setAttribute('name', p);
+      input.setAttribute('value', params[p]);
+      form.appendChild(input);
+    }
+  
+    // Add form to page and submit it to open the OAuth 2.0 endpoint.
+    document.body.appendChild(form);
+    form.submit();
+  }
+  // end of javascript
+
+  
