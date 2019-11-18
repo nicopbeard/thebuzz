ece049f219e7a1a5dbdbdb2d6d252e265821bcc8
nicopbeard <npb221@lehigh.edu>
2019-11-17 23:59:28 -0500

    Updated google drive api requests

--- backend/src/main/java/edu/lehigh/cse216/kta221/backend/App.java
+++ backend/src/main/java/edu/lehigh/cse216/kta221/backend/App.java
@@ -1,6 +1,5 @@
 package edu.lehigh.cse216.kta221.backend;
 
-import com.google.api.client.http.FileContent;
 import com.google.api.client.http.InputStreamContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
@@ -32,7 +31,6 @@ import net.rubyeye.xmemcached.utils.AddrUtil;
 import java.lang.InterruptedException;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -162,7 +160,6 @@ public class App {
             // if(!validToken(req.userId, req.googleToken)) {
             //     return gson.toJson(new StructuredMessageResponse(ERROR, TOKEN_ERROR));
             // }
-            System.out.println(req.file);
             if(req.fileName != null)
             {
                 try
@@ -369,45 +366,6 @@ public class App {
             }
         });
     }
-
-    /**
-   * Insert new file.
-   *
-   * @param service Drive API service instance.
-   * @param title Title of the file to insert, including the extension.
-   * @param description Description of the file to insert.
-   * @param parentId Optional parent folder's ID.
-   * @param mimeType MIME type of the file to insert.
-   * @param filename Filename of the file to insert.
-   * @return Inserted file metadata if successful, {@code null} otherwise.
-   */
-    // private static File insertFile(Drive service, String title, String description, String parentId, String mimeType, String filename) {
-    //     // File's metadata.
-    //     File body = new File();
-    //     body.setTitle(title);
-    //     body.setDescription(description);
-    //     body.setMimeType(mimeType);
-
-    //     // Set the parent folder.
-    //     if (parentId != null && parentId.length() > 0) {
-    //         body.setParents(Arrays.asList(new ParentReference().setId(parentId)));
-    //     }
-
-    //     // File's content.
-    //     java.io.File fileContent = new java.io.File(filename);
-    //     FileContent mediaContent = new FileContent(mimeType, fileContent);
-    //     try {
-    //         File file = service.files().insert(body, mediaContent).execute();
-
-    //         // Uncomment the following line to print the File ID.
-    //         //System.out.println("File ID: " + file.getId());
-
-    //         return file;
-    //     } catch (IOException e) {
-    //         System.out.println("An error occurred: " + e);
-    //         return null;
-    //     }
-    // }
 
     /**
      * Creates an authorized Credential object.
